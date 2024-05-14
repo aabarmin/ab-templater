173fce0042bd7524ae0fc15e82e227adde5cc012
Aleksandr Barmin <abarmin@outlook.com>
2024-05-14 12:26:20 +0100

    Initial commit

---
+++ src/test/java/dev/abarmin/templater/TemplaterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abarmin.templater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
